fb58233170f6e7dcb08e26656b961ee60ec019d9
Himorell <himorellj@gmail.com>
2022-12-28 20:22:07 +0100

    add some changes ShowTrip.php

--- resources/views/showTrip.blade.php
+++ resources/views/showTrip.blade.php
@@ -5,27 +5,31 @@
     <div>
         <img src="{{$trip->imgDestination}}" alt="Destination Image">
         <div>
-            <h2>{{$trip->originAddress}}</h2>
-            <h2>{{$trip->originPostcode}}</h2>
-            <h2>{{$trip->originCity}}</h2>
-            <h2>{{$trip->originCountry}}</h2>
-            <h2>{{$trip->destinationAddress}}</h2>
-            <h2>{{$trip->destinationPostcode}}</h2>
-            <h2>{{$trip->destinationCity}}</h2>
-            <h2>{{$trip->destinationCountry}}</h2>
-            <h2>{{$trip->preferences}}</h2>
-            <h2>{{$trip->seats}}</h2>
-            <h2>{{$trip->price}}</h2>
-            <h2>{{$trip->driverName}}</h2>
-            <h2>{{$trip->driverSurname}}</h2>
-            <h2>{{$trip->driverPhone}}</h2>
+            <div></div>
+            <h2>Trip Details</h2>
+            <h3>Origin Address: {{$trip->originAddress}}</h3>
+            <h3>Origin CP: {{$trip->originPostcode}}</h3>
+            <h3>Origin city: {{$trip->originCity}}</h3>
+            <h3>Origin country: {{$trip->originCountry}}</h3>
+            <h3>Destination address: {{$trip->destinationAddress}}</h3>
+            <h3>Destination CP: {{$trip->destinationPostcode}}</h3>
+            <h3>Destination city: {{$trip->destinationCity}}</h3>
+            <h3>Destination country: {{$trip->destinationCountry}}</h3>
+            <h3>Preferences: {{$trip->preferences}}</h3>
+            <h3>Seats: {{$trip->seats}}</h3>
+            <h3>Price: {{$trip->price}}</h3>
+            <h2>Driver details</h2>
+            <h3>Driver's name: {{$trip->driverName}}</h3>
+            <h3>Driver's surname: {{$trip->driverSurname}}</h3>
+            <h3>Driver's number phone: {{$trip->driverPhone}}</h3>
             <img src="{{$trip->driverImg}}" alt="Driver Image">
-            <h2>{{$trip->energyType}}</h2>
-            <h2>{{$trip->numberplate}}</h2>
-            <h2>{{$trip->vehicleType}}</h2>
-            <h2>{{$trip->date}}</h2>
-            <h2>{{$trip->departureTime}}</h2>
-            <h2>{{$trip->arrivalTime}}</h2>
+            <h2>Vehicle details</h2>
+            <h3>Vehicle energy type: {{$trip->energyType}}</h3>
+            <h3>Vehicle numberplate: {{$trip->numberplate}}</h3>
+            <h3>Vehicle type: {{$trip->vehicleType}}</h3>
+            <h3>{{$trip->date}}</h3>
+            <h3>{{$trip->departureTime}}</h3>
+            <h3>{{$trip->arrivalTime}}</h3>
         </div>
         <a href="{{route ('home')}}">↩</a>
     </div>
