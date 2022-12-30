8175557366e752e0c082a0f3479047432b71da4a
Himorell <himorellj@gmail.com>
2022-12-30 22:03:21 +0100

    Add some styles showTrip.php

--- resources/views/showTrip.blade.php
+++ resources/views/showTrip.blade.php
@@ -1,37 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div>
-        <img src="{{$trip->imgDestination}}" alt="Destination Image">
-        <div>
-            <div></div>
-            <h2>Trip Details</h2>
-            <h3>Origin Address: {{$trip->originAddress}}</h3>
-            <h3>Origin CP: {{$trip->originPostcode}}</h3>
-            <h3>Origin city: {{$trip->originCity}}</h3>
-            <h3>Origin country: {{$trip->originCountry}}</h3>
-            <h3>Destination address: {{$trip->destinationAddress}}</h3>
-            <h3>Destination CP: {{$trip->destinationPostcode}}</h3>
-            <h3>Destination city: {{$trip->destinationCity}}</h3>
-            <h3>Destination country: {{$trip->destinationCountry}}</h3>
-            <h3>Preferences: {{$trip->preferences}}</h3>
-            <h3>Seats: {{$trip->seats}}</h3>
-            <h3>Price: {{$trip->price}}</h3>
-            <h2>Driver details</h2>
-            <h3>Driver's name: {{$trip->driverName}}</h3>
-            <h3>Driver's surname: {{$trip->driverSurname}}</h3>
-            <h3>Driver's number phone: {{$trip->driverPhone}}</h3>
-            <img src="{{$trip->driverImg}}" alt="Driver Image">
-            <h2>Vehicle details</h2>
-            <h3>Vehicle energy type: {{$trip->energyType}}</h3>
-            <h3>Vehicle numberplate: {{$trip->numberplate}}</h3>
-            <h3>Vehicle type: {{$trip->vehicleType}}</h3>
-            <h3>{{$trip->date}}</h3>
-            <h3>{{$trip->departureTime}}</h3>
-            <h3>{{$trip->arrivalTime}}</h3>
-        </div>
-        <a href="{{route ('home')}}">↩</a>
+    
+    <img src="{{$trip->imgDestination}}" alt="Destination Image">
+    <div class="imgDestination" >
+        <h3>{{$trip->date}}</h3>
     </div>
+    
+    
+        <ul class="tripDetails">
+                <li>
+                    <div class="originTrip">
+                        <div class="departureTime">{{$trip->departureTime}}</div>
+                        <div class="lineDiv" aria-hidden="true"></div>
+                        <div class="origin">{{$trip->originAddress}}, CP: {{$trip->originPostcode}}. {{$trip->originCity}}-{{$trip->originCountry}}</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="destinationTrip">
+                        <div class="arrivalTime">{{$trip->arrivalTime}}</div>
+                        <div aria-hidden="true"></div>
+                        <div class="destination">{{$trip->destinationAddress}}, CP: {{$trip->destinationPostcode}}. {{$trip->destinationCity}}-{{$trip->destinationCountry}}</div>
+                    </div>
+                </li>
+        </ul>
+        <h3 class="priceTrip"> Cost per passenger: {{$trip->price}}€</h3>
+        <div class="summaryTrip">
+            <h3>Seats available: {{$trip->seats}}</h3>
+            <h3>Driver's name: {{$trip->driverName}}</h3>
+            <h3>Number phone: {{$trip->driverPhone}}</h3>
+            <img class="imgDriver" src="{{$trip->driverImg}}" alt="Driver Image">
+        </div>
+
+            <h3 class="preferences"> Preferences: <br> {{$trip->preferences}}</h3>
+            
+            <div class="vehicleDetails">
+                <h2>Vehicle details</h2>
+                <h3>Vehicle energy type: {{$trip->energyType}}</h3>
+                <h3>Vehicle numberplate: {{$trip->numberplate}}</h3>
+                <h3>Vehicle type: {{$trip->vehicleType}}</h3>
+            </div>
+            
+        
+        <a href="{{route ('home')}}">↩</a>
+    
     
 @endsection
