6a4579f41b6b1cae06dad6b45c493653cb1720ba
Beadeavila <beadeavila10@gmail.com>
2023-01-04 09:38:36 +0100

    home trip cards css started

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,27 +8,29 @@
     </button>
 </div>
 
-<div class="d-flex flex-wrap row justify-content-center my-4">
+<div class="list" >
     @foreach ($trips as $trip)
-        <div class="card" style="width: 12rem;">
+    <section class="cardHome">
+        <div class="card-body" >
             <a href="{{route ('showTrip', $trip->id)}}">
-                <img src="{{ $trip->imgDestination }}" class="card-img-top" alt="...">
-                <h5 class="card-title"> {{ $trip->date }} </h5>
-                <div class="card-body">
-                    <h5 class="card-title"> {{ $trip->departureTime }} {{ $trip->originCity }}</h5>
-                    <h5 class="card-title"> {{ $trip->arrivalTime }} {{ $trip->destinationCity }} </h5>
-                    <h5 class="card-title price"> {{ $trip->price }} € </h5>
-                    <h5 class="card-title"> {{ $trip->energyType }} </h5>
-                </div>
-            </a>
-            <form class=formActions action="{{ route('deleteTrip', ['id'=>$trip->id]) }}" method="POST">
-                @method('delete')
-                @csrf
-                <a href="{{route ('editTrip',['id'=>$trip->id])}}">✏</a>
-                <button type="submit" onclick="return confirm ('Are you sure you want to delete this trip to {{ $trip->destinationCity }}?')">🗑</button>
-            </form>
+            <img src="{{ $trip->imgDestination }}" class="imgDestination img-fluid h-100" alt="..."></a>
+            <h5 class="date"> {{ $trip->date }} </h5>
+            <h5 class="departureTime"> {{ $trip->departureTime }} </h5>
+            <h5 class="originCity"> {{ $trip->originCity }}</h5>
+            <h5 class="arrivalTime"> {{ $trip->arrivalTime }}</h5>
+            <h5 class="destinationCity"> {{ $trip->destinationCity }} </h5>
+            <h5 class="price col-9"> {{ $trip->price }} € </h5>
+            <h5 class="energyType"> {{ $trip->energyType }} </h5>
         </div>
+                <form class=formActions action="{{ route('deleteTrip', ['id'=>$trip->id]) }}" method="POST">
+                    @method('delete')
+                    @csrf
+                    <a href="{{route ('editTrip',['id'=>$trip->id])}}">✏</a>
+                    <button type="submit" onclick="return confirm ('Are you sure you want to delete this trip to {{ $trip->destinationCity }}?')">🗑</button>
+                </form>     
+    </section>
     @endforeach
 </div>
+
 
 @endsection
