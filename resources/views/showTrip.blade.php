6306840cb7918d2cd858d292664414d404b5a9f7
Himorell <himorellj@gmail.com>
2023-01-09 20:43:15 +0100

    finishing style showTrip.php

--- resources/views/showTrip.blade.php
+++ resources/views/showTrip.blade.php
@@ -8,7 +8,6 @@
     </div>
     <br>
     
-    <h2>Trip details</h2>
     <div class="tripDetails">
         <div class="timeDetails">
             <div class="departureTime">{{$trip->departureTime}}</div>
@@ -23,29 +22,25 @@
         </div>
     </div>
         <div class="seatsDetailsContainer">
-            <h3 class="seatsDetails"> Cost per passenger: {{$trip->price}}€</h3>
-            <h3 class="seatsDetails"> Seats available: {{$trip->seats}}</h3>  
+            <h4 class="seatsDetails"> Cost per passenger: {{$trip->price}}€</h4>
+            <h4 class="seatsDetails"> Seats available: {{$trip->seats}}</h4>  
         </div>
-        
-        <h2>Driver details</h2>
         <div class="driverDetails">
             <h4>Driver's name:  <br>{{$trip->driverName}}</h4> 
             <h4>Number phone: <br>{{$trip->driverPhone}}</h4> 
             <img class="imgDriver" src="{{$trip->driverImg}}" alt="Driver Image">
         </div>
-
             <div class="preferences"> 
                 <h4>Preferences:</h4>
                 <p>{{$trip->preferences}}</p> 
             </div> 
-            <h2>Vehicle details</h2>
             <div class="vehicleDetails">
-                <h3>Energy type: </h3>
-                <h4> {{$trip->energyType}}</h4>
-                <h3>Numberplate: </h3>
-                <h4>{{$trip->numberplate}}</h4>
-                <h3>Vehicle type: </h3>
-                <h4>{{$trip->vehicleType}}</h4>
+                <h4>Energy type: </h3>
+                <p> {{$trip->energyType}}</p>
+                <h4>Numberplate: </h4>
+                <p>{{$trip->numberplate}}</p>
+                <h4>Vehicle type: </h4>
+                <p>{{$trip->vehicleType}}</p>
             </div>
             
         
