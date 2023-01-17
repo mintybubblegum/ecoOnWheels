39baaacb176810c9922c4ef927fd9521b10f4a84
Adriana Aguilar <agruad@gmail.com>
2023-01-17 12:12:32 +0100

    updates pivot table delete

--- database/migrations/2023_01_13_093900_create_trip_user_table.php
+++ database/migrations/2023_01_13_093900_create_trip_user_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete("cascade");
 
             $table->unsignedBigInteger('trip_id');
-            $table->foreign('trip_id')->references('id')->on('trips');
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete("cascade");
 
             $table->timestamps();
         });
